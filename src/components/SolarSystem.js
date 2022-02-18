a26f92018493e0ecd2802ae09ed93e97991c17ca
wendril <wendrild.c.n@gmail.com>
2022-02-18 16:31:03 -0300

    Requisito 2 - Feito

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Header from './Header';
+
+class SolarSystem extends Component {
+  render() {
+    return (<div data-testid="solar-system"><Header /></div>);
+  }
+}
+
+export default SolarSystem;
