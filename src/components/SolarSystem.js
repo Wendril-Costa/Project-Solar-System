9fe0145b9b6441d806fc8cb02b0749c53a38aa99
wendril <wendrild.c.n@gmail.com>
2022-02-19 09:44:13 -0300

    Requisito 6 - Feito

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <div>
+          {planets.map((element) => (
+            <PlanetCard
+              key={ element.name }
+              planetName={ element.name }
+              planetImage={ element.image }
+            />
+          ))}
+        </div>
       </div>);
   }
 }
